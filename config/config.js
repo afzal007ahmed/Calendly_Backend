2ddcdd31ed1e75f54388df3c7c98743dd983f9d3
Afzal <afzalahmeddc@gmail.com>
2026-02-01 21:43:05 +0530

    added auth middleware

--- config/config.js
+++ config/config.js
@@ -8,6 +8,9 @@ const config = {
     name: process.env.DATABASE_NAME,
   },
   mongo_uri: `mongodb+srv://${process.env.MONGO_USER_NAME}:${process.env.MONGO_USER_PASSWORD}@cluster0.caabocs.mongodb.net/${process.env.DATABASE_NAME}?appName=Cluster0`,
+  jwt : {
+    secret : process.env.JWT_SECRET
+  }
 };
 
 module.exports = { config };
---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+const { config } = require("../config/config");
+
+const authMiddleware = (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    const err = new Error();
+    const jwtToken = token?.split(" ") ;
+    if (!token || !jwtToken[ 1 ]) {
+      err.message = "jwt token is missing.";
+      err.code = "TOKEN_MISSING";
+      throw err;
+    } else if (jwtToken[0] !== "Bearer") {
+      err.message = "please add Bearer in header.";
+      err.code = "BEARER_MISSING";
+      throw err;
+    }
+
+    req.user = jwt.verify(jwtToken[1], config.jwt.secret);
+    next();
+  } catch (err) {
+    res.status(401).send({
+      code: err.code || "OTHER",
+      message: err.message,
+    });
+  }
+};
+
+module.exports = { authMiddleware };
