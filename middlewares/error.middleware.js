fa8deae5a28a5c82f12e9c18bd201dfaafc379ea
Afzal <afzalahmeddc@gmail.com>
2026-02-01 21:44:33 +0530

    changes in error middleware

--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -1,11 +1,11 @@
 const errorMiddleware = (err, _req, res) => {
-  const statusCode = err.code;
+  const statusCode = err.statusCode;
+  const code = err.code;
   const message = err.message;
   res.status(statusCode).send({
     message: message,
+    code: code,
   });
 };
 
-
-
-module.exports = { errorMiddleware } ;
+module.exports = { errorMiddleware };
