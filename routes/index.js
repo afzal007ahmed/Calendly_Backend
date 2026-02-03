f2e751453500a7e530c082a0afba15bc24aea61f
mohdazaan <mohdazaan293@gmail.com>
2026-02-03 12:23:33 +0530

    Availability controllers

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,14 @@ const router = express.Router();
 
 const authRouter = require("./auth");
 const scheduleRouter = require("./schedule.route");
+const availabilityRouter = require('./availability.route')
 
 router.use("/auth", authRouter);
 
 router.use("/schedules", scheduleRouter);
 
 router.use("/meetings", require("./meeting.route"));
+
+router.use("/availability", availabilityRouter);
 
 module.exports = router;
